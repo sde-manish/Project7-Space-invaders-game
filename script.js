d7b9845dc2f48ed2be0cdbd0f66b75f1a836f6e8
Manish Kumar <manish481181@gmail.com>
2023-09-12 18:46:05 +0530

    setup

---
+++ script.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth;          //innerWidth is "window.innerwitdth". it's the same thing in javascript.
+canvas.height = innerHeight;
